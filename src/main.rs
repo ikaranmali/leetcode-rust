5dde064a48239cce12947ef80323bef32c6cf326
ikaranmali <ikaranmali@github.com>
2021-09-28 08:13:04 +0000

    added palindrome_check

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // mod two_sum;
-mod reverse_int;
+// mod reverse_int;
+mod palindrome_check;
 
 fn main(){
-    reverse_int::run_reverse_int();
-
+    // reverse_int::run_reverse_int();
     // two_sum::run_two_sum()
+    palindrome_check::run_is_palindrome();
+
 }
---
+++ src/palindrome_check.rs
@@ -0,0 +1,54 @@
+// Runtime: 0 ms, faster than 100.00% of Rust online submissions for Reverse Integer.
+// Memory Usage: 2.1 MB, less than 20.24% of Rust online submissions for Reverse Integer.
+
+struct Solution{
+    x: i32
+}
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+    let mut initial_x:i64 =x.into();
+    let mut num:i64 =x.into();
+    let mut reversed_integer:i64 = 0;
+    let mut rev_num:i64 = 0;
+    
+        // check if input number is in range of int32 then proceed for reverse
+        if x < 2147483647 && x > -2147483648{
+            // check if input number is negative if yes then set neg_sign flag true
+            if x < 0 {
+                return false
+                }
+            else{
+            
+                while num > 0{
+                    rev_num = rev_num *10 + num%10;
+                    num = num/10;
+                }
+                // convert rev_num to i32 from i64
+                reversed_integer = rev_num.into();
+                if reversed_integer < 2147483647 && reversed_integer > -2147483648 {
+                    if reversed_integer == initial_x{
+                        return true
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            }
+        }
+        
+        else{
+            return false
+        }    
+    }
+}
+
+
+pub fn run_is_palindrome(){
+   let sol =  Solution::is_palindrome(
+    151);
+   println!("{:?}",sol);
+}
